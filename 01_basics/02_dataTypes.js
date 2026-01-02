e54439afd92c8c9635dc1bebb8606f9439abc629
Abhishek-singh45 <7711abhishek@gmail.com>
2026-01-02 06:23:46 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+
+"use strict"; // treat all JS code as a newer version.
+
+let name =  "abhi"
+let age = 18
+let isLoggesIn = false
+
+// number => 2 to the power 53.
+// big data => use for large number. 
+// string => "" or '' more prefable is "" .
+// boolean => true or false.
+// null => standalone value . we can say object.
+// undefined => value is not assign.
+// symbol => use for unique.
+
+//OBJECT//
+
+console.log(typeof undefined); // output -> undefined.
+console.log(typeof null); // output -> object.
