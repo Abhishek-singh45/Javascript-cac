ca691c2f1c9f55eb0f48f2e49319392f845bdfe0
Abhishek-singh45 <7711abhishek@gmail.com>
2026-01-02 05:31:12 +0000

    Variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 32344
+ let accountEmail =  "abhi@google.com"
+ var accountPassward = "1234" // this way not recommended.
+ accountCity = "Gwalior" // this way also not recommended.
+ let accountState;
+
+
+ // accountId = 2 //-> this is not allowed because of accountId is constant.so id is not change/
+
+accountEmail = "ydiut5268"
+accountPassward = "696875"
+accountCity = "indore"
+
+console.log(accountId);
+
+console.table([accountEmail, accountPassward, accountCity, accountState])
