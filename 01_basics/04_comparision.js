eddc069381cc4d4650bf2c2899a9e8280b38cc9a
Abhishek-singh45 <7711abhishek@gmail.com>
2026-01-03 05:34:12 +0000

    Comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+//COMPARISIONS.
+
+console.log(2 > 1); //true
+console.log(2 >= 1); //true
+console.log(2 < 1); //false
+console.log(2 == 1); //false
+console.log(2 != 1); //true
+
+console.log("2" > 1); // automatically string change into a number they give output is true.
+
+// not predictable output
+console.log(null > 0);  // false
+console.log(null == 0); // false 
+console.log(null >= 0); // true 
+console.log(undefined > 0);  // false
+console.log(undefined == 0); // false 
+console.log(undefined >= 0); // false 
+
+//strict comparision -> ===
+
+console.log("2" === 2); //false because data type different  
+
