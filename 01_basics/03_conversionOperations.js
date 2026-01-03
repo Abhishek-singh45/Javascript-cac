8be21ea11af906b6fc412cb5361bf6d9ad8e7aa3
Abhishek-singh45 <7711abhishek@gmail.com>
2026-01-03 05:11:48 +0000

    Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,3 +1,5 @@
+//CONVERSION//
+
 let score = "33abc"
 
 console.log(typeof score); //simple way
@@ -26,3 +28,50 @@ let someNumbeer = 33
 let stringNumber = String(someNumbeer)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+//OPERATIONS//
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+//BASICS MATHS OPERATIONS//
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**2); // USE FOR POWER
+// console.log(2*2);
+// console.log(2/2);
+ 
+
+let str1 = "hello"
+let str2 = " abhi"
+
+let str3 = str1 + str2 
+console.log(str3);
+
+
+
+// if string first so all the content/no. trated as a string.
+
+console.log("1" + 2); // output -> 12
+console.log("1" + "2"); // output -> 12
+console.log("1" + 2 + 2); // output -> 122
+console.log(1 + 2 + "2" ); // output -> 32
+console.log(3*6+4-8/3); // not recommeded.
+
+
+console.log(+true); // output -> 1 , not recommeded
+console.log(+""); // output -> 0 , not recommeded
+
+
+//PREFIX AND POSTFIX 
+
+let gameCounter = 100
+++gameCounter; 
+console.log(gameCounter); // 101
+
+
+   
